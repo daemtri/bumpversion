7aaf6a8d914ae8f2475c1ddc624566e8c4a8e499
duanquanyong <duanquanyong@outlook.com>
2021-09-15 16:53:36 +0800

    fix: build tool

--- docker-build.sh
+++ docker-build.sh
@@ -1,2 +1,7 @@
-export BV_VERSION=`git describe --abbrev=0 --tags`
+#!/bin/bash
+[[ -z $(git status -s) ]] || (echo "git is not clean" && exit 1);
+export BV_VERSION=`git describe --tags`
 docker build --build-arg VERSION=$BV_VERSION -t harbor.bianfeng.com/library/bumpversion:$BV_VERSION .
+docker tag harbor.bianfeng.com/library/bumpversion:$BV_VERSION harbor.bianfeng.com/library/bumpversion:latest
+docker push harbor.bianfeng.com/library/bumpversion:$BV_VERSION
+docker push harbor.bianfeng.com/library/bumpversion:latest
